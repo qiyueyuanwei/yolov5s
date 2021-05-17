c256dfe78e9583a0f614184b5ba54f3508dc04b8
zhangsida <83306214@qq.com>
2021-05-17 16:52:58 +0800

    init

---
+++ YOLOV5/yolov5s/data/path.py
@@ -0,0 +1,16 @@
+from glob import glob
+import os
+
+
+#test_txt = './train.txt'
+test_txt = './valid_vis_random.txt'
+#path='E:/cars/cars_train/'
+path='/home/qiyue/YoloV5/images/val'
+files=os.listdir(path)
+line=[]
+
+
+for img in glob(os.path.join(path, '*.jpg')):
+    print(img)
+    with open(test_txt,'a') as f:
+        f.write('%s\n'%(img))
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker run -it --gpus all -v $PWD/images:/workspace/YOLOV5/images -v $PWD/labels:/workspace/YOLOV5/labels yolov5s:1.0 bash
